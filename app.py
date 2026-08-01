4ac8401a86e5b145bc038a2cb567854b8e133881
muhammadzahhid55-prog <muhammadzahhid55@gmail.com>
2026-08-01 13:25:21 +0500

    app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ import google.generativeai as genai
 GEMINI_API_KEY   = os.environ.get("GEMINI_API_KEY")
 WHATSAPP_TOKEN   = os.environ.get("WHATSAPP_TOKEN")
 PHONE_NUMBER_ID  = os.environ.get("PHONE_NUMBER_ID")
-VERIFY_TOKEN     = os.environ.get("VERIFY_TOKEN", "csa_verify_token")
+VERIFY_TOKEN     = os.environ.get("VERIFY_TOKEN", "csa2026secret")
 ADMIN_NUMBER     = os.environ.get("ADMIN_NUMBER", "923006498489")  # bina '+' aur bina space ke
 
 # Basic sanity check (Render logs mein dikhayega agar koi variable missing hai)
